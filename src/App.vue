94335de45546d36d80f63741f6dcc5669a61e8b2
chrizzis <christian.d.seabrook@gmail.com>
2021-08-19 17:14:16 -0700

    add gh-pages templates n deploy and stub layout

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,89 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <v-app id="app">
+    <v-app-bar hide-on-scroll :input-value.sync="showNavs" app>
+      <v-app-bar-nav-icon
+        @click.stop="toggleNavDrawer = !toggleNavDrawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-capitalize">{{ title }}</v-toolbar-title>
     </v-app-bar>
+    <v-navigation-drawer v-model="toggleNavDrawer" fixed temporary>
+      <v-list dense nav>
+        <v-list-item
+          v-for="action in navDrawerActions"
+          :key="action.title"
+          :to="action.route"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-{{ action.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title class="text-capitalize">
+              {{ action.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
+
+    <v-bottom-navigation hide-on-scroll :input-value.sync="showNavs" grow app>
+      <v-btn
+        v-for="action in navDrawerActions"
+        :key="action.title"
+        :to="action.route"
+        :input-value="isActiveNav(action.title)"
+      >
+        <span class="text-capitalize">{{ action.title }}</span>
+        <v-icon>mdi-{{ action.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
-
 <script>
-
 export default {
-  name: 'App',
-
   data: () => ({
-    //
+    title: "welcome to the demo",
+    toggleNavDrawer: false,
+    showNavs: true,
+    navDrawerActions: [
+      {
+        title: "home",
+        icon: "home",
+        route: "/",
+      },
+      {
+        title: "about",
+        icon: "information",
+        route: "/about",
+      },
+    ],
+    contextActions: [
+      {
+        title: "home",
+        icon: "home",
+        route: "/",
+      },
+    ],
   }),
+
+  computed: {},
+
+  methods: {
+    isActiveNav(key) {
+      return key === this.$route.name;
+    },
+  },
+
+  watch: {
+    $route(to) {
+      // react to route changes...
+      this.title = to.name;
+      this.showNavs = true;
+    },
+  },
 };
 </script>
